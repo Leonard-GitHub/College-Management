2cfd0ecd3c83510708a0800449a933bf1ea354e7
LEONARD ALPS <leonardalps@gmail.com>
2022-01-19 11:27:38 +0530

    commit

--- app/src/main/java/com/example/collegemanagement/HomeFragment.java
+++ app/src/main/java/com/example/collegemanagement/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.collegemanagement;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +9,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class HomeFragment extends Fragment {
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = (ViewGroup)  inflater.inflate(R.layout.fragment_home,container,false);
+        return view;
 
-        ViewGroup root = (ViewGroup)  inflater.inflate(R.layout.fragment_home,container,false);
 
-        return root;
+
     }
+
 }
--- app/src/main/java/com/example/collegemanagement/SplashScreen.java
+++ app/src/main/java/com/example/collegemanagement/SplashScreen.java
@@ -3,11 +3,15 @@ package com.example.collegemanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
+
+    final String PREFS_NAME = "MyPrefsFile";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +22,38 @@ public class SplashScreen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashScreen.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-
+                firstTime();
             }
         },2000);
 
+
+    }
+
+
+
+    public  void  firstTime() {
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        if (settings.getBoolean("my_first_time", true)) {
+            //the app is being launched for first time, do something
+            Intent intent = new Intent(SplashScreen.this, IntroActivity.class);
+            startActivity(intent);
+            finish();
+
+            // first time task
+
+            // record the fact that the app has been started at least once
+            settings.edit().putBoolean("my_first_time", false).commit();
+        } else {
+            Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
     }
 }
