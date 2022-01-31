7cb22de54fec6784c7f90110d4cfc5eeddcd60d6
LEONARD ALPS <leonardalps@gmail.com>
2022-01-31 23:24:58 +0530

    commit

--- app/src/main/java/com/example/collegemanagement/MainActivity.java
+++ app/src/main/java/com/example/collegemanagement/MainActivity.java
@@ -8,15 +8,20 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.yarolegovich.slidingrootnav.SlidingRootNav;
@@ -24,20 +29,28 @@ import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
 import java.util.Arrays;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener{
 
     private Button btnSignOut;
     private GoogleSignInClient mGoogleSignInClient;
 
+    TextView email, name;
+    CircleImageView userProfileImage;
 
-    private static final int POS_CLOSE=0;
-    private static final int POS_HOME = 1;
-    private static final int POS_NOTES = 2;
-    private static final int POS_VIDEO_LECTURES = 3;
-    private static final int POS_QUESTION_PAPER = 4;
-    private static final int POS_TO_DO_LIST = 5;
-    private static final int POS_ABOUT = 7;
-    private static final int POS_LOGOUT = 8;
+
+
+    private static final int POS_HOME = 0;
+    private static final int POS_NOTES = 1;
+    private static final int POS_VIDEO_LECTURES = 2;
+    private static final int POS_QUESTION_PAPER = 3;
+    private static final int POS_TO_DO_LIST = 4;
+    private static final int POS_ABOUT = 6;
+    private static final int POS_LOGOUT = 7;
+
+    Toolbar toolbar;
+
 
 
     private String[] screenTitles;
@@ -50,11 +63,11 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         slidingRootNav = new SlidingRootNavBuilder(this)
-                .withDragDistance(180)
+                .withDragDistance(225)
                 .withRootViewScale(0.75f)
                 .withRootViewElevation(25)
                 .withToolbarMenuToggle(toolbar)
@@ -71,7 +84,6 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnI
 
 
         DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
-                createItemFor(POS_CLOSE),
                 createItemFor(POS_HOME).setChecked(true),
                 createItemFor(POS_NOTES),
                 createItemFor(POS_VIDEO_LECTURES),
@@ -91,6 +103,22 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnI
         adapter.setSelected(POS_HOME);
 
 
+
+        //for usr profile and name
+        userProfileImage = findViewById(R.id.image_usr);
+        email = findViewById(R.id.email_address);
+        name = findViewById(R.id.name_usr);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personFamilyName = acct.getFamilyName();
+            String personGivenName = acct.getGivenName();
+            String personEmail = acct.getEmail();
+            Uri personPhoto = acct.getPhotoUrl();
+            email.setText(personEmail);
+            name.setText(personGivenName);
+            Glide.with(this).load(String.valueOf(personPhoto)).into(userProfileImage);
+        }
 
 
     }
@@ -152,35 +180,41 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnI
         FragmentTransaction transition  = getSupportFragmentManager().beginTransaction();
 
         if (position == POS_HOME) {
+            toolbar.setTitle("HOME");
             HomeFragment homeFragment = new HomeFragment();
             transition.replace(R.id.container, homeFragment);
         }
 
 
         else if (position == POS_NOTES) {
+            toolbar.setTitle("NOTES");
             NotesFragment notesFragment =  new NotesFragment();
             transition.replace(R.id.container, notesFragment);
         }
 
 
         else if (position == POS_VIDEO_LECTURES) {
+            toolbar.setTitle("Video Lectures");
            VideoLecturesFragment videoLecturesFragment = new VideoLecturesFragment();
             transition.replace(R.id.container, videoLecturesFragment);
         }
 
 
         else if (position == POS_QUESTION_PAPER) {
+            toolbar.setTitle("Question Papers");
             QuestionPaperFragment questionPaperFragment = new QuestionPaperFragment();
             transition.replace(R.id.container, questionPaperFragment);
         }
 
 
         else if (position == POS_TO_DO_LIST) {
+            toolbar.setTitle("To-Do List");
             TodoListFragment todoListFragment = new TodoListFragment();
             transition.replace(R.id.container, todoListFragment);
         }
 
         else if (position == POS_ABOUT) {
+            toolbar.setTitle("About");
             AboutUsFragment aboutUsFragment = new AboutUsFragment();
             transition.replace(R.id.container, aboutUsFragment);
         }
