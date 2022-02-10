5ca40ba166093d3160f919f03b68b1aadb287bdc
LEONARD ALPS <leonardalps@gmail.com>
2022-02-10 16:19:57 +0530

    commit 1

--- app/src/main/java/com/example/collegemanagement/LoginActivity.java
+++ app/src/main/java/com/example/collegemanagement/LoginActivity.java
@@ -67,7 +67,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void Glogin() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.default_web))
                 .requestEmail()
                 .build();
 
