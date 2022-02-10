4ad246ef37edf9981e29b81ced00700f2a87c95d
LEONARD ALPS <leonardalps@gmail.com>
2022-02-10 18:28:33 +0530

    commit 8

--- app/src/main/java/com/example/collegemanagement/ProfileActivity.java
+++ app/src/main/java/com/example/collegemanagement/ProfileActivity.java
@@ -16,31 +16,5 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-
-        profileToolbar=findViewById(R.id.profile_toolbar);
-        profileToolbar.setTitle("Profile");
-        setSupportActionBar(profileToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-
-
-
-
-        //for usr profile and name
-        email = findViewById(R.id.usr_email);
-        name = findViewById(R.id.name_usr);
-        userProfileImage = findViewById(R.id.profile_image);
-        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-        if (acct != null) {
-            String personName = acct.getDisplayName();
-            String personFamilyName = acct.getFamilyName();
-            String personGivenName = acct.getGivenName();
-            String personEmail = acct.getEmail();
-            Uri personPhoto = acct.getPhotoUrl();
-            email.setText(personEmail);
-            name.setText(personGivenName);
-            Glide.with(this).load(String.valueOf(personPhoto)).into(userProfileImage);
-        }
     }
 }
